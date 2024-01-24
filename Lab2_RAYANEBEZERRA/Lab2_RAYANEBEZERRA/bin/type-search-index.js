03da424abdfb728e8928bc97fcd7609b173ee87f
Rayane Bezerra <rayane.silva@ccc.ufcg.edu.br>
2024-01-24 16:19:26 -0300

    fazendo modificacoes

--- Lab2_RAYANEBEZERRA/Lab2_RAYANEBEZERRA/bin/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lab2","l":"AtividadesComplementares"},{"p":"lab2","l":"Coisa"},{"p":"lab2","l":"Descanso"},{"p":"lab2","l":"Disciplina"},{"p":"lab2","l":"RegistroTempoOnline"}];updateSearchResults();
